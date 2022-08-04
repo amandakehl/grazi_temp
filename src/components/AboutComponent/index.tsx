396535cae12e992eec2602517fc83bb401a5c250
Debora <debora.pozzebon@gmail.com>
2022-08-04 13:50:13 -0300

    rel="noreferrer"

--- src/components/AboutComponent/index.tsx
+++ src/components/AboutComponent/index.tsx
@@ -13,10 +13,10 @@ export default function AboutComponent() {
         <span className={styles.description}>Após trabalhar 6 anos na advocacia tradicional, descobri meu propósito na área: proporcionar clareza e acesso,  meu próprio estilo de trabalho elaborando contratos com linguagem clara e objetiva.</span>
         <span>Sem juridiquês e sem palavras difíceis para que todos entendam o que está sendo proposto e assim, trazer mais segurança jurídica para o seu negócio.</span>
         <div className={styles.buttons}>
-          <a href='https://www.instagram.com/grazielamatana/' target='_blank'>
+          <a href='https://www.instagram.com/grazielamatana/' rel="noreferrer" target='_blank'>
             <AiFillInstagram color='#CC6F84' />
           </a>
-          <a href='https://www.linkedin.com/in/graziela-matana-bb347b1b5/' target='_blank'>
+          <a href='https://www.linkedin.com/in/graziela-matana-bb347b1b5/' rel="noreferrer" target='_blank'>
             <AiFillLinkedin color='#CC6F84' />
           </a>
         </div>
